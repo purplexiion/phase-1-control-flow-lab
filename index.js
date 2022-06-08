40316a5c5d7818dd9cc68bc916c250a867274f95
Wamuyu <michellewachira25@gmail.com>
2022-06-08 20:18:20 +0300

    commit tree d65876a5b5a5f75adbfdef0aa5b3a80ee3bcdaf0

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  // else if (ride >2500)
+  // return result = "No can do.";
+//
+
+let feet = 199
+function scuberGreetingForFeet(feet){
+  if (feet <= 400 ){
+  return "This one is on me!";}
+  else if (feet < 2500){
+    return "I will gladly take your thirty bucks."
+    }
+  else {return "No can do."}
+}
+console.log(scuberGreetingForFeet(2500));
+
+let City = "NYC";
+function ternaryCheckCity(City){
+  const result =(City === "NYC")? "Ok, sounds good." : "No go.";
+  return result;
+  }
+
+
+let Tip = "Generous";
+function switchOnCharmFromTip(Tip){
+  switch (Tip){
+  case "generous":
+  return ("Thank you so much.");
+  break
+    case "not as generous":
+      return ("Thank you.");
+      break
+      default:
+        return ("Bye.");
+}
 }
